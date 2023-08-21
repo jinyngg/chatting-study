01e3cdeb3bc17fea1001c492f8099ed478fec7c9
jyjang <jinyngg@gmail.com>
2023-08-22 02:29:58 +0900

    [#1] feat : WebSocketConfig 작성

---
+++ src/main/java/com/study/chat/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.study.chat.config;
+
+import com.study.chat.handler.ChatHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatHandler chatHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "ws/chat")
+                .setAllowedOriginPatterns("*");
+    }
+}
