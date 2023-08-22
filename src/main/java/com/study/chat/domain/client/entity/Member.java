8a6696985f70de733202047ab31a001fb4bdc83f
jyjang <jinyngg@gmail.com>
2023-08-22 17:28:58 +0900

    [#3] feat : 회원 entity 추가

---
+++ src/main/java/com/study/chat/domain/client/entity/Member.java
@@ -0,0 +1,26 @@
+package com.study.chat.domain.client.entity;
+
+import com.study.chat.common.entity.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+}
